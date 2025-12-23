1dae34a12fa6627d92a7da470458753ebc871235
CarlesRa <jcramos8282@gmail.com>
2025-12-23 12:35:57 +0100

    Vite plugin inspect

--- React-app/vite.config.ts
+++ React-app/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { WatermarkPlugin } from './plugins/WatermarkPlugin';
+import Inspect from 'vite-plugin-inspect'
 
 function manualChunks(id: any) {
   if (id.includes('node_modules')) {
@@ -17,6 +18,7 @@ export default defineConfig({
       color: 'red',
       text: 'test stage'
     }),
+    Inspect(),
   ],
   build: {
     rollupOptions: {
